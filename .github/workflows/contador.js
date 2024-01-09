472ca1a8fa6c522c3bb7dd2a60018cc881591fe3
LucasRodrigues020 <70955498+LucasRodrigues020@users.noreply.github.com>
2024-01-09 00:03:14 -0300

    Add files via upload

---
+++ .github/workflows/contador.js
@@ -0,0 +1,7 @@
+// No seu arquivo contador.js
+let contador = 0;
+
+function incrementarContador() {
+    contador++;
+    // Adicione aqui o código para enviar o contador para algum lugar (pode ser um servidor, armazenamento na nuvem, etc.)
+}
